2464f7e404153e668e6de50e283c35613b92278d
Emil Hafeez <eh2928@cumc.columbia.edu>
2021-01-25 22:14:49 -0500

    add data, read in, polish code, begin running

---
+++ p8158_hw2_eh2928.R
@@ -0,0 +1,17 @@
+library(psych)
+library(GPArotation)
+
+### Read in the genetic testing data
+dat <- read.csv("/Users/emilhafeez/Google Drive/Columbia/Spring 2021/Latent Variables and SEM/Homework 2/p8158_hw2_eh2928/genetictestingrawdata.csv",head = TRUE,sep = ",")
+
+### Grab just the financial features
+features = dat[,c(2, 4, 5, 6, 15)]
+
+### Write descriptive stats to screen
+summary(features)
+
+### Here we use polychoric correlation and do parallel analysis of eigenvalues
+geneticparallel<-fa.parallel(features, cor=”poly”, fa=”pc”)
+
+geneticparallel$pc.values
+geneticparallel$pc.sim
